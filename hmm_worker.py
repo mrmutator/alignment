ed1164354d21bc6aefbfffdbc4a3d8a555cd5172
Roger Wechsler <mr.mutator@gmail.com>
2016-02-18 00:10:40 +0100

    Implemented parallel version.

---
+++ hmm_worker.py
@@ -0,0 +1,120 @@
+from Corpus_Reader import Corpus_Reader
+from collections import defaultdict, Counter
+import numpy as np
+import cPickle as pickle
+import multiprocessing as mp
+
+
+def train_iteration(corpus, trans_prob, al_prob, results):
+    # set all counts to zero
+    counts_e_f = Counter()
+    counts_e = Counter()
+    pi_counts = Counter() # (i, I)
+    pi_denom = Counter() # I
+    xi_sums = Counter() # (i, i_p, I)
+    gamma_sums = Counter() # (i_p, I)
+    ll = 0
+    for e_toks, f_toks in corpus:
+        I = len(e_toks)
+        J = len(f_toks)
+        alphas = np.zeros((J, I))
+        betas = np.zeros((J, I))
+        for j, f_tok in enumerate(f_toks):
+            for i, e_tok in enumerate(e_toks):
+                t_f_e = trans_prob[e_tok][f_tok]
+                if j == 0:
+                    alphas[j][i] = t_f_e * al_prob[(None, I)][i]
+                else:
+                    alphas[j][i] = t_f_e * np.sum([alphas[j - 1][k] * al_prob[I][i - k] for k in xrange(I)])
+
+                # lexical translation parameters
+                delta_t = t_f_e / np.sum([trans_prob[k_tok][f_tok] for k_tok in e_toks])
+                counts_e_f[(e_tok, f_tok)] += delta_t
+                counts_e[e_tok] += delta_t
+
+        for j, f_tok in reversed(list(enumerate(f_toks))):
+            for i, e_tok in enumerate(e_toks):
+                if j == J-1:
+                    betas[j][i] = 1
+                else:
+                    betas[j][i] = np.sum([betas[j+1][k] * trans_prob[e_k][f_toks[j+1]] * al_prob[I][k-i] for k, e_k in enumerate(e_toks)])
+
+        # posteriors
+        multiplied = np.multiply(alphas, betas)
+        denom_sums = 1.0 / np.sum(multiplied, axis=1)
+
+        gammas = multiplied * denom_sums[:, np.newaxis]
+
+        for j in xrange(1, J):
+            t_f_e = np.array([trans_prob[e_tok][f_toks[j]] for e_tok in e_toks])
+            beta_t_j_i = np.multiply(betas[j], t_f_e)
+            j_p = j-1
+            alpha_j_p = alphas[j_p]
+            denom = np.sum(np.multiply(alpha_j_p, betas[j_p]))
+            for i_p in range(I):
+                alpha_j_p_i_p = alpha_j_p[i_p]
+                gamma_sums[(i_p, I)] += gammas[j_p][i_p]
+                for i in range(I):
+                    xi = (al_prob[I][i - i_p]  * alpha_j_p_i_p * beta_t_j_i[i]) / denom
+                    xi_sums[(i,i_p, I)] += xi
+        # add counts
+        for i in range(I):
+            pi_counts[(i, I)] += gammas[0][i]
+        pi_denom[I] += 1
+
+        ll += np.sum(alphas[J-1])
+
+
+    results.put([counts_e_f, counts_e, gamma_sums, xi_sums, pi_counts, pi_denom, ll])
+
+
+def load_probs(trans_probs):
+    probs = dict()
+    for (cond,x), v in trans_probs.iteritems():
+        if cond not in probs:
+            probs[cond] = dict()
+        probs[cond][x] = v
+    return probs
+
+
+
+e_file = "test/tp.e.1"
+f_file = "test/tp.f.1"
+params_file = "test/tp.prms.1"
+output_exp_file = "test/tp.exp.1"
+num_workers = 6
+
+
+corpus = Corpus_Reader(e_file, f_file)
+trans_params, al_params = pickle.load(open(params_file, "rb"))
+
+trans_prob = load_probs(trans_params)
+al_prob = load_probs(al_params)
+
+corpus = list(corpus)
+n= len(corpus) / num_workers
+data = [corpus[i:i+n] for i in range(0, len(corpus), n)]
+
+results = mp.Queue()
+
+processes = [mp.Process(target=train_iteration, args=(data[i], trans_prob, al_prob, results)) for i in xrange(num_workers)]
+for p in processes:
+    p.start()
+
+initial = results.get()
+total = initial[:-1]
+total_ll = initial[-1]
+
+for p in processes[1:]:
+    counts = results.get()
+    for i, c in enumerate(counts[:-1]):
+        total[i].update(c)
+    total_ll += counts[-1]
+for p in processes:
+    a = p.join()
+
+
+expectations = {'counts_e_f': total[0], 'counts_e':total[1], 'gamma_sums': total[2], 'xi_sums': total[3],
+                'pi_counts':total[4], 'pi_denom':total[5], 'll':total_ll}
+
+pickle.dump(expectations, open(output_exp_file, "wb"))
---
+++ update_parameters.py
@@ -0,0 +1,58 @@
+import cPickle as pickle
+from collections import defaultdict, Counter
+
+
+def load_params(param_file):
+    trans_params, al_params = pickle.load(open(param_file, "rb"))
+    return trans_params, al_params
+
+
+exp_files = ["test/tp.exp.1"]
+param_files = ["test/tp.prms.1"]
+
+
+total = defaultdict(Counter)
+
+total_ll = 0
+
+for f in exp_files:
+    expectations = pickle.load(open(f, "rb"))
+
+    total_ll += expectations["ll"]
+    del expectations["ll"]
+
+    for count, d in expectations.iteritems():
+        total[count].update(d)
+
+print total["counts_e"][1]
+
+print "LL before update: ", total_ll
+
+trans_prob = dict()
+al_prob = dict()
+
+# update parameters
+
+for (e,f), count in total['counts_e_f'].items():
+    trans_prob[(e,f)] = count / total['counts_e'][e]
+
+jmp_dict = defaultdict(lambda: defaultdict(int))
+for (i, i_p, I), count in total['xi_sums'].items():
+    jmp_dict[I][i-i_p] += count / total['gamma_sums'][(i_p, I)]
+for I_p in jmp_dict:
+    norm_c = sum(jmp_dict[I_p].values())
+    for jmp in jmp_dict[I_p]:
+        al_prob[(I_p, jmp)] = jmp_dict[I_p][jmp] / norm_c
+
+for (i, I), count in total['pi_counts'].items():
+    al_prob[((None, I), i)] = count / total['pi_denom'][I]
+
+
+for f in param_files:
+    trans_params, al_params = load_params(f)
+    for k in trans_params:
+        trans_params[k] = trans_prob[k]
+    for k in al_params:
+        al_params[k] = al_prob[k]
+
+    pickle.dump((trans_params, al_params), open(f +".u", "wb"))
