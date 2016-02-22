e5a5f77064037c6550bcc2846acc9ea6574dd57e
Roger Wechsler <mr.mutator@gmail.com>
2016-02-22 13:00:47 +0100

    Implemented null_words for class variant.

--- hmm_jumps.py
+++ hmm_jumps.py
@@ -9,7 +9,8 @@ import multiprocessing as mp
 
 class HMMAlignment(AlignmentModel):
 
-    def __init__(self, trans_prob=None, al_prob=None):
+    def __init__(self, trans_prob=None, al_prob=None, p_0=0.2):
+        self.p_0 = p_0 # null word transition probability
         # initialize t(f|e) randomly
         # t(f|e) = trans_prob[e][f]
         if not trans_prob:
@@ -93,19 +94,31 @@ class HMMAlignment(AlignmentModel):
             # e_toks = [None] + e_toks # introduce NULL-token
             I = len(e_toks)
             J = len(f_toks)
-            alphas = np.zeros((J, I))
-            betas = np.zeros((J, I))
+            alphas = np.zeros((J, 2*I))
+            betas = np.zeros((J, 2*I))
             scale_coeffs = np.zeros(J)
             for j, f_tok in enumerate(f_toks):
-                for i, e_tok in enumerate(e_toks):
+                for i, e_tok in enumerate(e_toks + [0] * I):
                     t_f_e = self.trans_prob[e_tok][f_tok]
-                    if j == 0:
-                        alphas[j][i] = t_f_e * self.al_prob[(None, I)][i]
+                    if e_tok == 0:
+                        # i is NULL
+                        if j == 0:
+                            alphas[j][i] = t_f_e * self.p_0
+                        else:
+                            # go to NULL_i is only possible from NULL_i or from NULL_i - I
+                            alphas[j][i] = t_f_e * np.sum([alphas[j - 1][k] * self.p_0 for k in [i-I, i]])
                     else:
-                        alphas[j][i] = t_f_e * np.sum([alphas[j - 1][k] * self.al_prob[I][i - k] for k in xrange(I)])
+                        # i is not null
+                        if j == 0:
+                            alphas[j][i] = t_f_e * self.al_prob[(None, I)][i]
+                        else:
+                            alphas[j][i] = t_f_e * (np.sum([alphas[j - 1][k] * self.al_prob[I][i - k]
+                                                            for k in xrange(I)]) + np.sum([alphas[j-1][k] *
+                                                                    self.al_prob[I][i - k + I] for k in range(I, 2*I)]))
+
 
                     # lexical translation parameters
-                    delta_t = t_f_e / np.sum([self.trans_prob[k_tok][f_tok] for k_tok in e_toks])
+                    delta_t = t_f_e / np.sum([self.trans_prob[k_tok][f_tok] for k_tok in e_toks + [0]*I])
                     counts_e_f[(e_tok, f_tok)] += delta_t
                     counts_e[e_tok] += delta_t
                 # rescale alphas for numerical stability
@@ -114,11 +127,25 @@ class HMMAlignment(AlignmentModel):
                 scale_coeffs[j] = Z
 
             for j, f_tok in reversed(list(enumerate(f_toks))):
-                for i, e_tok in enumerate(e_toks):
-                    if j == J-1:
-                        betas[j][i] = 1
+                for i, e_tok in enumerate(e_toks + [0] * I):
+                    if e_tok == 0:
+                        # i is NULL
+                        if j == J-1:
+                            betas[j][i] = 1
+                        else:
+                            betas[j][i] = np.sum([betas[j+1][k] * self.trans_prob[e_k][f_toks[j+1]] *
+                                                  self.al_prob[I][k-i+I] for k, e_k in enumerate(e_toks)]) +\
+                                          (betas[j+1][i] * self.trans_prob[0][f_toks[j+1]] *
+                                                  self.p_0)
                     else:
-                        betas[j][i] = np.sum([betas[j+1][k] * self.trans_prob[e_k][f_toks[j+1]] * self.al_prob[I][k-i] for k, e_k in enumerate(e_toks)])
+                        # i is not NULL
+                        if j == J-1:
+                            betas[j][i] = 1
+                        else:
+                            betas[j][i] = np.sum([betas[j+1][k] * self.trans_prob[e_k][f_toks[j+1]] *
+                                                  self.al_prob[I][k-i] for k, e_k in enumerate(e_toks)]) + \
+                                          (betas[j+1][i+I] * self.trans_prob[0][f_toks[j+1]] *
+                                                  self.p_0)
                 if j != J-1:
                     # rescale betas for numerical stability
                     betas[j] = betas[j] / scale_coeffs[j+1]
@@ -132,20 +159,28 @@ class HMMAlignment(AlignmentModel):
             gammas = np.multiply(alphas, betas)
 
             for j in xrange(1, J):
-                t_f_e = np.array([self.trans_prob[e_tok][f_toks[j]] for e_tok in e_toks])
+                t_f_e = np.array([self.trans_prob[e_tok][f_toks[j]] for e_tok in e_toks + [0]*I])
                 beta_t_j_i = np.multiply(betas[j], t_f_e)
                 j_p = j-1
                 alpha_j_p = alphas[j_p]
                 # denom = np.sum(np.multiply(alpha_j_p, betas[j_p]))
-                for i_p in range(I):
+                for i_p in range(2*I):
                     alpha_j_p_i_p = alpha_j_p[i_p]
                     gamma_sums[(i_p, I)] += gammas[j_p][i_p]
-                    for i in range(I):
-                        # xi = (self.al_prob[I][i - i_p]  * alpha_j_p_i_p * beta_t_j_i[i]) / denom
-                        xi = (self.al_prob[I][i - i_p]  * alpha_j_p_i_p * beta_t_j_i[i]) / scale_coeffs[j]
-                        xi_sums[(i,i_p, I)] += xi
+                    for i in range(2*I):
+                        if i >= I:
+                            if i - I == i_p or i == i_p: # i is NULL
+                                xi = (self.p_0  * alpha_j_p_i_p * beta_t_j_i[i]) / scale_coeffs[j]
+                                xi_sums[(i,i_p, I)] += xi
+                        else:
+                            if i_p < I:
+                                xi = (self.al_prob[I][i - i_p]  * alpha_j_p_i_p * beta_t_j_i[i]) / scale_coeffs[j]
+                                xi_sums[(i,i_p, I)] += xi
+                            else:
+                                xi = (self.al_prob[I][i - i_p + I]  * alpha_j_p_i_p * beta_t_j_i[i]) / scale_coeffs[j]
+                                xi_sums[(i,i_p-I, I)] += xi
             # add counts
-            for i in range(I):
+            for i in range(2*I):
                 pi_counts[(i, I)] += gammas[0][i]
             pi_denom[I] += 1
 
@@ -159,35 +194,56 @@ class HMMAlignment(AlignmentModel):
         """
         J = len(f_toks)
         I = len(e_toks)
-        alphas = np.zeros((J, I))
+        alphas = np.zeros((J, 2*I))
         for j, f_tok in enumerate(f_toks):
-            for i, e_tok in enumerate(e_toks):
-                if j == 0:
-                    alpha_j_i = self.trans_prob[e_tok][f_tok] * self.al_prob[(None, I)][i]
+            for i, e_tok in enumerate(e_toks + [0]*I):
+                t_f_e = self.trans_prob[e_tok][f_tok]
+                if e_tok == 0:
+                    # i is NULL
+                    if j == 0:
+                        alphas[j][i] = t_f_e * self.p_0
+                    else:
+                        # go to NULL_i is only possible from NULL_i or from NULL_i - I
+                        alphas[j][i] = t_f_e * np.sum([alphas[j - 1][k] * self.p_0 for k in [i-I, i]])
                 else:
-                    alpha_j_i = self.trans_prob[e_tok][f_tok] * np.sum([ alphas[j-1][k]*self.al_prob[I][i-k] for k in range(I)])
-                alphas[j][i] = alpha_j_i
+                    # i is not null
+                    if j == 0:
+                        alphas[j][i] = t_f_e * self.al_prob[(None, I)][i]
+                    else:
+                        alphas[j][i] = t_f_e * (np.sum([alphas[j - 1][k] * self.al_prob[I][i - k]
+                                                        for k in xrange(I)]) + np.sum([alphas[j-1][k] *
+                                                                self.al_prob[I][i - k + I] for k in range(I, 2*I)]))
 
         sent_prob = np.sum(alphas[J-1])
         return np.log(sent_prob)
 
     def get_viterbi_alignment(self, e_toks, f_toks):
-        alignments = []
         J = len(f_toks)
         I = len(e_toks)
-        chart = np.zeros((J, I))
-        best = np.zeros((J, I))
+        chart = np.zeros((J, 2*I))
+        best = np.zeros((J, 2*I))
         # initialize
         for i, e_tok in enumerate(e_toks):
             chart[0][i] = self.trans_prob[e_tok][f_toks[0]] * self.al_prob[(None, I)][i]
+        for i in range(I, I*2):
+            chart[0][i] = self.trans_prob[0][f_toks[0]] * self.p_0
 
         # compute chart
         for j, f_tok in enumerate(f_toks[1:]):
             j= j+1
-            for i, e_tok in enumerate(e_toks):
+            for i, e_tok in enumerate(e_toks + [0]*I):
                 values = []
-                for i_p in range(I):
-                    values.append(chart[j-1][i_p]*self.al_prob[I][i-i_p])
+                for i_p in range(2*I):
+                    if i >= I:
+                        if i - I == i_p or i == i_p: # i is NULL
+                            values.append(chart[j-1][i_p] * self.p_0)
+                        else:
+                            values.append(0)
+                    else:
+                        if i_p < I:
+                            values.append(chart[j-1][i_p]*self.al_prob[I][i-i_p])
+                        else:
+                            values.append(chart[j-1][i_p]*self.al_prob[I][i-i_p + I])
                 best_i = np.argmax(values)
                 chart[j][i] = values[best_i] * self.trans_prob[e_tok][f_tok]
                 best[j][i] = best_i
@@ -201,7 +257,7 @@ class HMMAlignment(AlignmentModel):
             best_path.append(best_end)
 
         best_path = list(reversed(best_path))
-        alignments = [(int(best_path[j]), j) for j in range(J)]
+        alignments = [(int(best_path[j]), j) for j in range(J) if int(best_path[j]) < I]
         return alignments
 
     def test_probs(self):
@@ -218,9 +274,9 @@ class HMMAlignment(AlignmentModel):
 if __name__ == "__main__":
     # create corpus instance
     corpus = Corpus_Reader("test/tp.1.e", "test/tp.1.f", limit=100)
-    trans_params, al_params = pickle.load(open("test/tp.1.prms", "rb"))
+    #trans_params, al_params = pickle.load(open("test/tp.1.prms", "rb"))
     # create model, omit parameters for random initialization
-    model = HMMAlignment(al_prob=al_params, trans_prob=trans_params)
+    model = HMMAlignment(al_prob=None, trans_prob=None)
 
     # train model until convergence delta is small enough or max_iterations is reached
     model.train(corpus, max_iterations=5, convergence_ll=0.001)
