6abd3b4b30c892b6bc91e61451b045357c8e56e4
Roger Wechsler <mr.mutator@gmail.com>
2016-04-19 13:34:10 +0200

    Adapted head-annotation tool for visualization.

--- word_alignment/parsing/annotate_corpus_heads.py
+++ word_alignment/parsing/annotate_corpus_heads.py
@@ -1,15 +1,11 @@
-# annotate original F-corpus file with heads from snt-parsed (5-lines) file
 import codecs
 
+
 class CorpusReader(object):
-    def __init__(self, corpus_file, limit=None, return_order=False):
+    def __init__(self, corpus_file, limit=None):
         self.corpus_file = open(corpus_file, "r")
         self.limit = limit
         self.next = self.__iter_sent
-        if return_order:
-            self.buffer_end = 4
-        else:
-            self.buffer_end = 3
 
     def reset(self):
         self.corpus_file.seek(0)
@@ -23,14 +19,13 @@ class CorpusReader(object):
             if b != -1:
                 buffer.append(map(int, line.strip().split()))
             b += 1
-            if b == 4:
-                yield buffer[:self.buffer_end]
+            if b == 7:
+                yield buffer
                 c += 1
                 if c == self.limit:
                     break
                 b = -1
                 buffer = []
-
 
     def __iter__(self):
         return self.next()
@@ -41,28 +36,28 @@ class CorpusReader(object):
             c += 1
         return c
 
+
 if __name__ == "__main__":
     import argparse
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("f_raw")
-    arg_parser.add_argument("snt_file")
+    arg_parser.add_argument("psnt_file")
 
     args = arg_parser.parse_args()
 
     infile = codecs.open(args.f_raw, "r", "utf-8")
     outfile = codecs.open(args.f_raw + ".annotated", "w", "utf-8")
-    corpus = CorpusReader(args.snt_file, return_order=True)
-    for _, _, heads, order in corpus:
+    corpus = CorpusReader(args.psnt_file)
+    for _, _, heads, _, _, _, order in corpus:
         toks = infile.readline().strip().split()
         assert len(toks) == len(heads)
         pairs = zip(heads, order)
         new_heads, _ = zip(*sorted(pairs, key=lambda t: t[1]))
         new_heads = map(order.__getitem__, new_heads)
-        new_heads[order[0]] = -1 # root
+        new_heads[order[0]] = -1  # root
         annotated = [t + "_" + str(new_heads[i]) for i, t in enumerate(toks)]
         outfile.write(" ".join(annotated) + "\n")
 
     infile.close()
     outfile.close()
-
-
