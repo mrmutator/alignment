4025310764c7a61d96d2b25d17e06cefb3d12959
Roger Wechsler <mr.mutator@gmail.com>
2016-04-19 08:47:38 +0200

    Improved evaluation script.

--- word_alignment/utils/evaluate.py
+++ word_alignment/utils/evaluate.py
@@ -1,17 +1,19 @@
 import argparse
 
-class Evaluation(object):
 
-    def __init__(self, gold_file, alignment_file, gold_order=('e', 'f'), al_order=('e', 'f')):
+class Evaluation(object):
+    def __init__(self, gold_file, alignment_file=None, gold_order=('e', 'f'), al_order=('e', 'f')):
         self.alignments = set()
         self.sure = set()
         self.probable = set()
+        self.gold_file = gold_file
 
-        self.read_alignment_file(alignment_file, order=al_order)
+        if alignment_file:
+            self.read_alignment_file(alignment_file, order=al_order)
+            self.alignment_file = alignment_file
         self.read_gold_file(gold_file, order=gold_order)
 
-
-    def read_gold_file(self, file_name, order=("e","f")):
+    def read_gold_file(self, file_name, order=("e", "f")):
         with open(file_name, "r") as infile:
             for line in infile:
                 els = line.strip().split()
@@ -33,16 +35,17 @@ class Evaluation(object):
                     self.sure.add(tpl)
                     self.probable.add(tpl)
 
-
     def read_alignment_file(self, file_name, order=('e', 'f')):
+        self.alignments = set()
+        self.alignment_file = file_name
         with open(file_name, "r") as infile:
             for i, line in enumerate(infile):
                 als = [tuple(x.split("-")) for x in line.strip().split()]
                 for al in als:
                     if order == ("e", "f"):
-                        tpl = (i+1, int(al[0])+1, int(al[1])+1)
+                        tpl = (i + 1, int(al[0]) + 1, int(al[1]) + 1)
                     elif order == ("f", "e"):
-                        tpl = (i+1, int(al[1])+1, int(al[0])+1)
+                        tpl = (i + 1, int(al[1]) + 1, int(al[0]) + 1)
                     else:
                         raise Exception("Invalid order type.")
                     self.alignments.add(tpl)
@@ -56,17 +59,23 @@ class Evaluation(object):
     def f1_measure(self):
         p = self.precision()
         r = self.recall()
-        return (2*p*r) / (p + r)
+        return (2 * p * r) / (p + r)
 
     def aer(self):
         return 1 - ((len(self.alignments.intersection(self.sure)) + len(self.alignments.intersection(self.probable)))
                     / float(len(self.alignments) + len(self.sure)))
 
+    def print_machine_output(self):
+        print "\t".join([self.alignment_file] + map("{:6.4f}".format, [round(self.precision(), 4),
+                                                                       round(self.recall(), 4),
+                                                                       round(self.f1_measure(), 4),
+                                                                       round(self.aer(), 4)]))
+
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-gold", required=True)
-    arg_parser.add_argument('-test', required=True)
+    arg_parser.add_argument('-test_files', nargs='+')
     arg_parser.add_argument('-swap_order', dest='swap_order', action='store_true', default=False)
 
     args = arg_parser.parse_args()
@@ -74,18 +83,9 @@ if __name__ == "__main__":
     if args.swap_order:
         gold_order = ('f', 'e')
     else:
-        gold_order= ('e', 'f')
+        gold_order = ('e', 'f')
 
-    eval = Evaluation(args.gold, args.test, gold_order=gold_order)
-    print "GOLD:", args.gold
-    print "TEST:", args.test
-    print
-    print "P:   ", round(eval.precision(), 4)
-    print "R:   ", round(eval.recall(), 4)
-    print "F1:  ", round(eval.f1_measure(), 4)
-    print "AER: ", round(eval.aer(), 4)
-
-
-
-
-
+    eval = Evaluation(args.gold, gold_order=gold_order)
+    for fname in args.test_files:
+        eval.read_alignment_file(fname)
+        eval.print_machine_output()
