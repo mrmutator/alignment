81f77e10b43f16a4d073ac6b14de834e36a46bc7
Roger Wechsler <roger.wechsler@student.uva.nl>
2016-07-21 15:35:26 +0200

    Modified annotate corpus.

--- word_alignment/parsing/annotate_corpus_heads.py
+++ word_alignment/parsing/annotate_corpus_heads.py
@@ -53,29 +53,43 @@ if __name__ == "__main__":
     arg_parser.add_argument("f_raw")
     arg_parser.add_argument("psnt_file")
     arg_parser.add_argument("-pos_voc", required=False, default="")
+    arg_parser.add_argument("-rel_voc", required=False, default="")
+    arg_parser.add_argument('-hmm_params', dest='hmm_params', action='store_true', default=False)
 
     args = arg_parser.parse_args()
 
     if args.pos_voc:
         pos_dict = read_pos_voc(args.pos_voc)
+    if args.rel_voc:
+        rel_dict = read_pos_voc(args.rel_voc)
 
     infile = codecs.open(args.f_raw, "r", "utf-8")
     outfile = codecs.open(args.f_raw + ".annotated", "w", "utf-8")
     corpus = CorpusReader(args.psnt_file)
-    for _, _, heads, pos, _, _, order in corpus:
+    for _, _, heads, pos, rel, hmm_params, order in corpus:
         toks = infile.readline().strip().split()
         assert len(toks) == len(heads)
         pairs = zip(heads, order)
+        annotations = ["" for t in toks]
         if args.pos_voc:
             pos_pairs = zip(pos, order)
             new_pos, _ = zip(*sorted(pos_pairs, key=lambda t: t[1]))
-            new_pos = ["/" + pos_dict.get(t) for t in new_pos]
-        else:
-            new_pos = ["" for t in toks]
+            annotations = ["/" + pos_dict.get(t) for t in new_pos]
+        if args.rel_voc:
+            rel_pairs = zip(rel, order)
+            new_rel, _ = zip(*sorted(rel_pairs, key=lambda t: t[1]))
+            for i, t in enumerate(annotations):
+                annotations[i] = t +"/" + rel_dict.get(new_rel[i])
+        if args.hmm_params:
+            hmm_pairs = zip(hmm_params, order)
+            new_hmm, _ = zip(*sorted(hmm_pairs, key=lambda t: t[1]))
+            for i, t in enumerate(annotations):
+                annotations[i] = t + "_" + str(new_hmm[i])
+
         new_heads, _ = zip(*sorted(pairs, key=lambda t: t[1]))
         new_heads = map(order.__getitem__, new_heads)
         new_heads[order[0]] = -1  # root
-        annotated = [t + new_pos[i] +  "_" + str(new_heads[i]) for i, t in enumerate(toks)]
+        annotated = [t + annotations[i] +  "_" + str(new_heads[i]) for i, t in enumerate(toks)]
         outfile.write(" ".join(annotated) + "\n")
 
     infile.close()
