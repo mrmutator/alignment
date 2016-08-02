19f5a59bed7a900367bc6555505e817d2189a901
Roger Wechsler <roger.wechsler@student.uva.nl>
2016-08-02 11:38:26 +0200

    Optimizing feature extraction. Work in progress.

--- word_alignment/models/feature_model/extract_template.py
+++ word_alignment/models/feature_model/extract_template.py
@@ -3,30 +3,67 @@ from CorpusReader import CorpusReader
 import features as fm
 import gzip
 import numpy as np
+from collections import defaultdict
 
-def read_feature_file(file_name):
-    features = dict()
-    features["standard"] = dict()
-    features["start"] = dict()
-    features["standard"][frozenset([("empty", 0)])] = 0
-    features["start"][frozenset([("empty", 0)])] = 0
-    c = 0
-    with open(file_name, "r") as infile:
-        for line in infile:
-            if line.strip():
-                f_pairs = line.strip().split("\t")
-                f_pairs = [tuple(p.split(" ")) for p in f_pairs]
-                subfeatures = frozenset([(k, int(v)) for k,v in f_pairs])
-                if ("j", 0) in subfeatures:
-                    if subfeatures not in features["start"]:
-                        c += 1
-                        features["start"][subfeatures] = c
-                else:
-                    if subfeatures not in features["standard"]:
-                        c += 1
-                        features["standard"][subfeatures] = c
+class FeatureStore(object):
 
-    return features
+    def __init__(self, feature_file):
+        self.index = 0
+        self.lookup = dict()
+        self.features = dict()
+        self.read_feature_file(feature_file)
+
+
+    def add_key(self, k):
+        self.lookup[k] = defaultdict(set)
+        self.lookup[k][None] = set(self.features.keys())
+
+
+    def add_features(self, features):
+        self.index += 1
+        curr_id = self.index
+        features = dict(features)
+        for k in features:
+            if k not in self.lookup:
+                self.add_key(k)
+
+        self.features[curr_id] = features
+        for k in self.lookup:
+            if k in features:
+                self.lookup[k][features[k]].add(curr_id)
+            else:
+                self.lookup[k][None].add(curr_id)
+
+    def read_feature_file(self, fn):
+        collected = set()
+        with open(fn, "r") as infile:
+            for line in infile:
+                if line.strip():
+                    f_pairs = line.strip().split("\t")
+                    f_pairs = [tuple(p.split(" ")) for p in f_pairs]
+                    subfeatures = frozenset([(k, int(v)) for k, v in f_pairs])
+                    if subfeatures not in collected:
+                        self.add_features(subfeatures)
+                        collected.add(subfeatures)
+
+
+class ExtractedFeatures(object):
+
+    def __init__(self, feature_store, extracted_features=None):
+        self.feature_store = feature_store
+        if extracted_features:
+            self.extracted_features = extracted_features.extracted_features
+        else:
+            self.extracted_features = set(self.feature_store.features.keys())
+
+    def add_feature(self, tpl):
+        k,v = tpl
+        if k in self.feature_store.lookup:
+            valid = self.feature_store.lookup[k][v].union(self.feature_store.lookup[k][None])
+            self.extracted_features = self.extracted_features.intersection(valid)
+
+    def get_feature_ids(self):
+        return frozenset(self.extracted_features)
 
 
 def extract_features(corpus, feature_pool, out_file_name):
@@ -60,32 +97,29 @@ def extract_features(corpus, feature_pool, out_file_name):
 
 
         # sentence_level
-        features_sentence_level = set()
-        features_sentence_level.add(("I", I))
-        features_sentence_level.add(("J", J))
+        features_sentence_level = ExtractedFeatures(feature_pool)
+        features_sentence_level.add_feature(("I", I))
+        features_sentence_level.add_feature(("J", J))
 
 
         # j=0
         j=0
-        features_0 = set(features_sentence_level)
+        features_0 = ExtractedFeatures(feature_pool, features_sentence_level)
         j_tree_level = 0
-        features_0.add(("cpos", pos[j]))
-        features_0.add(("crel", rel[j]))
-        features_0.add(("cdir", dir[j]))
-        features_0.add(("ctl", j_tree_level))
-        features_0.add(("clc", left_children[j]))
-        features_0.add(("crc", right_children[j]))
-        features_0.add(("cc", children[j]))
-        features_0.add(("j", j))
-        features_0.add(("oj", order[j]))
+        features_0.add_feature(("cpos", pos[j]))
+        features_0.add_feature(("crel", rel[j]))
+        features_0.add_feature(("cdir", dir[j]))
+        features_0.add_feature(("ctl", j_tree_level))
+        features_0.add_feature(("clc", left_children[j]))
+        features_0.add_feature(("crc", right_children[j]))
+        features_0.add_feature(("cc", children[j]))
+        features_0.add_feature(("j", j))
+        features_0.add_feature(("oj", order[j]))
 
-        conditions = []
-        for cand, cand_i in feature_pool["start"].iteritems():
-            if cand.issubset(features_0):
-                conditions.append(cand_i)
+        conditions = features_0.get_feature_ids()
         if not conditions:
-            conditions = [0]
-        condition_id = con_ids.get_id(frozenset(conditions))
+            conditions = frozenset([0])
+        condition_id = con_ids.get_id(conditions)
         vectors = []
         for i in xrange(I):
             features_i = set()
@@ -98,55 +132,52 @@ def extract_features(corpus, feature_pool, out_file_name):
             vector_id = vector_ids.get_id(frozenset(features_i))
             vectors.append(vector_id)
 
-        # do all the wirting here
+        # do all the writing here
         outfile.write(" ".join(map(str, [condition_id] + vectors)) + "\n")
 
 
         # rest
         for j in xrange(1, J):
-            features_j = set(features_sentence_level)
+            features_j = ExtractedFeatures(feature_pool, features_sentence_level)
             # add features
             h = f_heads[j]
             j_tree_level = tree_levels[h] + 1
             tree_levels[j] = j_tree_level
-            features_j.add(("ppos", pos[h]))
-            features_j.add(("prel", rel[h]))
-            features_j.add(("pdir", dir[h]))
-            features_j.add(("cpos", pos[j]))
-            features_j.add(("crel", rel[j]))
-            features_j.add(("cdir", dir[j]))
-            features_j.add(("l", order[j]-order[h]))
-            features_j.add(("absl", abs(order[j]-order[h])))
-            features_j.add(("ctl", j_tree_level))
-            features_j.add(("ptl", tree_levels[j]))
-            features_j.add(("plc", left_children[h]))
-            features_j.add(("prc", right_children[h]))
-            features_j.add(("pc", children[h]))
-            features_j.add(("clc", left_children[j]))
-            features_j.add(("crc", right_children[j]))
-            features_j.add(("cc", children[j]))
-            features_j.add(("j", j))
-            features_j.add(("pj", h))
-            features_j.add(("oj", order[j]))
-            features_j.add(("op", order[h]))
-            features_j.add(("phmm", hmm_transitions[h]))
-            features_j.add(("chmm", hmm_transitions[j]))
+            features_j.add_feature(("ppos", pos[h]))
+            features_j.add_feature(("prel", rel[h]))
+            features_j.add_feature(("pdir", dir[h]))
+            features_j.add_feature(("cpos", pos[j]))
+            features_j.add_feature(("crel", rel[j]))
+            features_j.add_feature(("cdir", dir[j]))
+            features_j.add_feature(("l", order[j]-order[h]))
+            features_j.add_feature(("absl", abs(order[j]-order[h])))
+            features_j.add_feature(("ctl", j_tree_level))
+            features_j.add_feature(("ptl", tree_levels[j]))
+            features_j.add_feature(("plc", left_children[h]))
+            features_j.add_feature(("prc", right_children[h]))
+            features_j.add_feature(("pc", children[h]))
+            features_j.add_feature(("clc", left_children[j]))
+            features_j.add_feature(("crc", right_children[j]))
+            features_j.add_feature(("cc", children[j]))
+            features_j.add_feature(("j", j))
+            features_j.add_feature(("pj", h))
+            features_j.add_feature(("oj", order[j]))
+            features_j.add_feature(("op", order[h]))
+            features_j.add_feature(("phmm", hmm_transitions[h]))
+            features_j.add_feature(("chmm", hmm_transitions[j]))
 
 
             for i_p in xrange(I):
-                features_i_p = set(features_j)
+                features_i_p = ExtractedFeatures(feature_pool, features_j)
                 # add featueres
-                features_i_p.add(("ip", i_p))
+                features_i_p.add_feature(("ip", i_p))
 
                 # static features complete
                 # check if there is a feature that matches
-                conditions = []
-                for cand, cand_i in feature_pool["standard"].iteritems():
-                    if cand.issubset(features_i_p):
-                        conditions.append(cand_i)
+                conditions = features_i_p.get_feature_ids()
                 if not conditions:
-                    conditions = [0]
-                condition_id = con_ids.get_id(frozenset(conditions))
+                    conditions = frozenset([0])
+                condition_id = con_ids.get_id(conditions)
                 vectors = []
                 for i in xrange(I):
                     features_i = set()
@@ -185,6 +216,6 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
     corpus = CorpusReader(args.corpus)
 
-    feature_pool = read_feature_file(args.feature_file)
+    feature_pool = FeatureStore(args.feature_file)
 
     extract_features(corpus, feature_pool, args.corpus)
