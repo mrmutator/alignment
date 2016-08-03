e85a7986f3deafcc711816c6dfec0cfcdd45a506
Roger Wechsler <roger.wechsler@student.uva.nl>
2016-08-03 21:35:02 +0200

    Optimized single worker.

--- word_alignment/models/feature_model/single_worker.py
+++ word_alignment/models/feature_model/single_worker.py
@@ -12,99 +12,90 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s  %(message)s')
 logger = logging.getLogger()
 
+def all_traces(x):
+    jj = np.tile(np.arange(x.shape[1]),x.shape[0])
+    ii = (np.arange(x.shape[1])+np.arange(x.shape[0])[::-1,None]).ravel()
+    z = np.zeros(((x.shape[0]+x.shape[1]-1),x.shape[1]))
+    z[ii,jj] = x.ravel()
+    return z.sum(axis=1)
 
-def train_iteration(buffer, p_0, trans_params, dist_vecs, dist_weights):
+
+def train_iteration(buffer, p_0, trans_params, dist_vecs, dist_weights, start_counts, al_counts):
     # set all counts to zero
     lex_counts = Counter()  # (e,f)
     lex_norm = Counter()  # e
-    al_counts = Counter()  # (static_cond, dynamic_cond)
     ll = 0
     norm_coeff = 1.0 - p_0
     SMALL_PROB_CONST = 0.00000001
 
     feature_dim = len(dist_weights)
 
-    #for buff in buffer:
     for (e_toks, f_toks, f_heads, feature_ids) in buffer:
         I = len(e_toks)
         I_double = 2 * I
         J = len(f_toks)
-
 
         translation_matrix = np.zeros((J, I_double))
         marginals = np.zeros((J, I_double))
         # start probs
         # i_p is 0 for start_probs
         feature_matrix = lil_matrix((I, feature_dim))
-        f_0 = f_toks[0]
-        for i in xrange(I):
-            translation_matrix[0][i] = trans_params.get((e_toks[i], f_0), SMALL_PROB_CONST) # abuse for loop
-            features_i = dist_vecs[feature_ids[0][0][1][i]]
+        t_params_j = t_params.get(f_toks[0], {})
+        translation_matrix[0][I:] = t_params_j.get(0, SMALL_PROB_CONST)
+        static = dist_vecs[feature_ids[0]]
+        for i, e_tok in enumerate(e_toks):
+            translation_matrix[0][i] = t_params_j.get(e_tok, SMALL_PROB_CONST)
+            features_i = static[i]
             feature_matrix.rows[i] = features_i
             feature_matrix.data[i] = [1.0] * len(features_i)
-        feature_matrix = feature_matrix.tocsr()
-        numerator = np.exp(feature_matrix.dot(dist_weights))
+        feature_matrix2 = feature_matrix.tocsr()
+        numerator = np.exp(feature_matrix2.dot(dist_weights))
         s_probs = (numerator / np.sum(numerator)) * norm_coeff
-        marginals[0]= np.hstack((s_probs, np.ones(I) * (p_0 / I)))
+        marginals[0] = np.hstack((s_probs, np.ones(I) * (p_0 / I)))
 
         # dist probs
-        d_probs = np.zeros((J-1, I_double, I_double))
+        d_probs = np.zeros((J - 1, I_double, I_double))
         tmp = np.hstack((np.zeros((I, I)), np.identity(I) * p_0))
         template = np.vstack((tmp, tmp))
-        translation_matrix[0][I:] = trans_params.get((0, f_0), SMALL_PROB_CONST) # null word for first word
         for j in xrange(1, J):
-            f_j = f_toks[j]
-            translation_matrix[j][I:] = trans_params.get((0, f_j), SMALL_PROB_CONST)
-            #temp_probs = np.zeros((I_double, I_double))
-            for i_p in xrange(I):
-                feature_matrix = lil_matrix((I, feature_dim))
-                translation_matrix[j][i_p] = trans_params.get((e_toks[i_p], f_j), SMALL_PROB_CONST)
+            t_params_j = t_params.get(f_toks[j], {})
+            translation_matrix[j][I:] = t_params_j.get(0, SMALL_PROB_CONST)
+            for i_p, e_tok in enumerate(e_toks):
+                translation_matrix[j][i_p] = t_params_j.get(e_tok, SMALL_PROB_CONST)
+                static = dist_vecs[feature_ids[j][i_p]]
                 for i in xrange(I):
-                    features_i = dist_vecs[feature_ids[j][i_p][1][i]]
+                    features_i = static[i-i_p]
                     feature_matrix.rows[i] = features_i
                     feature_matrix.data[i] = [1.0] * len(features_i)
-                feature_matrix = feature_matrix.tocsr()
-                num = np.exp(feature_matrix.dot(dist_weights))
-                d_probs[j-1, i_p, :I] = num
-                d_probs[j-1, i_p +I, :I] = num
+                feature_matrix2 = feature_matrix.tocsr()
+                num = np.exp(feature_matrix2.dot(dist_weights))
+                d_probs[j - 1, i_p, :I] = num
+                d_probs[j - 1, i_p + I, :I] = num
 
-        d_probs = ((d_probs / np.sum(d_probs, axis=2)[:, :, np.newaxis]) * norm_coeff) + template
+        dist_probs = ((d_probs / np.sum(d_probs, axis=2)[:, :, np.newaxis]) * norm_coeff) + template
 
-        gammas, xis, pair_ll = hmt.upward_downward(J, I_double, f_heads, translation_matrix, d_probs,
+        gammas, xis, log_likelihood = hmt.upward_downward(J, I_double, f_heads, translation_matrix, dist_probs,
                                                    marginals)
 
         # update counts
-
-        # add start counts and counts for lex f_0
-        for j_p, f_tok in enumerate(f_toks[1:]):
-            j = j_p+1
-            gammas_0_j = np.sum(gammas[j][I:])
-            if translation_matrix[j, I] > SMALL_PROB_CONST:
-                lex_counts[(0, f_tok)] += gammas_0_j
-            for i, e_tok in enumerate(e_toks):
-                if translation_matrix[j, i] > SMALL_PROB_CONST:
-                    lex_counts[(e_tok, f_tok)] += gammas[j][i]
-                for i_p in range(I):
-                    static_cond = feature_ids[j][i_p][0]
-                    dynamic_cond = feature_ids[j][i_p][1][i]
-                    al_counts[(static_cond, dynamic_cond)] += xis[j][i_p][i] + xis[j][i_p+I][i]
-
-        f_0 = f_toks[0]
-        if translation_matrix[0, I] > SMALL_PROB_CONST:
-            lex_counts[(0, f_0)] += np.sum(gammas[0][I:])
-        e_norm = np.sum(gammas, axis=0)
-        static_cond = feature_ids[0][0][0]
+        gammas[:, I] = np.sum(gammas[:, I:], axis=1)
+        e_norm = np.sum(gammas[:,:I+1], axis=0)
+        e_toks += [0]
         for i, e_tok in enumerate(e_toks):
             lex_norm[e_tok] += e_norm[i]
-            dynamic_cond = feature_ids[0][0][1][i]
-            al_counts[(static_cond, dynamic_cond)] += gammas[0][i]
-            if translation_matrix[0, i] > SMALL_PROB_CONST:
-                lex_counts[(e_tok, f_0)] += gammas[0][i]
-        lex_norm[0] += np.sum(e_norm[I:])
+            for j, f_tok in enumerate(f_toks):
+                if translation_matrix[j, i] > SMALL_PROB_CONST:
+                    lex_counts[(e_tok, f_tok)] += gammas[j][i]
 
-        ll += pair_ll
+        start_counts[feature_ids[0]][:I] += gammas[0][:I]
+        for j, ips in enumerate(feature_ids[1:]):
+            xis[j + 1][:I][:I] += xis[j + 1][I:][:I]
+            for ip, static in enumerate(ips):
+                al_counts[static][ip, :I] += xis[j + 1][ip][:I]
 
-    return lex_counts, lex_norm, al_counts, ll
+        ll += log_likelihood
+
+    return lex_counts, lex_norm, ll
 
 
 def load_params(file_name):
@@ -117,7 +108,9 @@ def load_params(file_name):
             e = int(els[1])
             f = int(els[2])
             p = float(els[3])
-            t_params[(e, f)] = p
+            if f not in t_params:
+                t_params[f] = dict()
+            t_params[f][e] = p
         else:
             raise Exception("Should not happen.")
     infile.close()
@@ -129,9 +122,11 @@ def load_vecs(file_name):
     infile = open(file_name, "r")
     for line in infile:
         els = line.strip().split()
-        cid = els[0]
-        feature_ids = map(int, els[1:])
-        vec_ids[cid] = frozenset(feature_ids)
+        jmp, cid = els[0].split(".")
+        if cid not in vec_ids:
+            vec_ids[cid] = dict()
+
+        vec_ids[cid][int(jmp)] = sorted(map(int, els[1:]))
     infile.close()
     return vec_ids
 
@@ -146,9 +141,8 @@ def load_weights(file_name):
     return np.array(d_weights)
 
 
-def writing_counts(total, total_ll, counts_file):
-    # types = ["lex_counts", "lex_norm", "al_counts"]
-    types = map(str, range(3))
+def writing_counts(total,  start_counts, al_counts, total_ll, counts_file):
+    types = map(str, range(2))
     with open(counts_file, "w") as outfile:
         outfile.write("LL:\t" + str(total_ll) + "\n")
         for i, counter in enumerate(total):
@@ -160,7 +154,30 @@ def writing_counts(total, total_ll, counts_file):
                     k = str(k)
                 v = str(v)
                 outfile.write("\t".join([t, k, v]) + "\n")
+
+        for con, array in start_counts.iteritems():
+            I = len(array)
+            for i in xrange(I):
+                outfile.write("\t".join(["2", con +" "+str(i), str(array[i])]) + "\n")
+        for con, array in al_counts.iteritems():
+            I = len(array)
+            diagonal_sums = all_traces(array)
+            for i, jmp in enumerate(xrange(-I+1, I)):
+                outfile.write("\t".join(["2", con+" "+str(jmp), str(diagonal_sums[i])]) + "\n")
     logger.info("Counts written.")
+
+def load_convoc(convoc_file):
+    with open(convoc_file, "r") as infile:
+        al_counts = dict()
+        start_counts = dict()
+        for line in infile:
+            t, con, max_I = line.split()
+            max_I = int(max_I)
+            if t == "s":
+                start_counts[con] = np.zeros(max_I)
+            elif t == "j":
+                al_counts[con] = np.zeros((max_I, max_I))
+    return start_counts, al_counts
 
 
 #############################################
@@ -171,27 +188,24 @@ if __name__ == "__main__":
     arg_parser.add_argument("-params", required=True)
     arg_parser.add_argument("-vecs", required=True)
     arg_parser.add_argument("-weights", required=True)
+    arg_parser.add_argument("-convoc", required=True)
     arg_parser.add_argument("-p_0", required=False, type=float, default=0.2)
 
     args = arg_parser.parse_args()
 
     counts_file_name = args.params + ".counts"
 
-    vec_ids = load_vecs(args.vecs)
-    d_weights = load_weights(args.weights)
-
-
     corpus = SubcorpusReader(args.corpus)
-
 
     logger.info("Loading parameters.")
     t_params = load_params(args.params)
+    vec_ids = load_vecs(args.vecs)
+    d_weights = load_weights(args.weights)
+    start_counts, al_counts = load_convoc(args.convoc)
 
-
-    #corpus_buffer = Corpus_Buffer(corpus, buffer_size=args.buffer_size)
-    logger.info("Starting worker processes.")
-    lex_counts, lex_norm, al_counts, ll = train_iteration(corpus, args.p_0, t_params, vec_ids, d_weights)
+    logger.info("Running training.")
+    lex_counts, lex_norm,  ll = train_iteration(corpus, args.p_0, t_params, vec_ids, d_weights, start_counts, al_counts)
 
     logger.info("Writing counts to file.")
-    writing_counts([lex_counts, lex_norm, al_counts], ll, re.sub("\.params\.", ".counts.", args.params))
+    writing_counts([lex_counts, lex_norm], start_counts, al_counts, ll, re.sub("\.params\.", ".counts.", args.params))
     logger.info("Done.")
