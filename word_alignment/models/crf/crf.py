fd087f6a2170b34494d9eb280401f66a173d2165
Roger Wechsler <roger.wechsler@student.uva.nl>
2016-08-13 10:37:34 +0200

    Updates.

--- word_alignment/models/crf/crf.py
+++ word_alignment/models/crf/crf.py
@@ -62,8 +62,6 @@ def datapoint_worker(process_queue, result_queue):
                 feature_matrices[j].append(ip_feature_matrix)
                 d_probs[j - 1, ip, :I+1] =  np.exp(ip_feature_matrix.dot(weights))
 
-        #dist_probs = (d_probs / np.sum(d_probs, axis=2)[:, :, np.newaxis])
-
         gammas, xis, Z = hmt.upward_downward(J, I_ext, f_heads, d_probs,
                                                    marginals)
 
@@ -81,6 +79,81 @@ def datapoint_worker(process_queue, result_queue):
 
         result_queue.put((Z, gold_ll, gradient))
 
+def predict(process_queue, result_queue):
+    dist_vecs = dict(vec_ids)
+    feature_dim = len(global_weights)
+    while True:
+        buffer = process_queue.get()
+        if buffer is None:
+            return
+
+        (I, f_heads, gold_aligned, feature_ids), weights = buffer
+        # set all counts to zero
+
+        I_ext = I + 1
+        J = len(f_heads)
+
+
+        feature_matrix = lil_matrix((I_ext, feature_dim))
+        for i in xrange(I_ext):
+            features_i = dist_vecs[feature_ids[0][i]]
+            feature_matrix.rows[i] = features_i
+            feature_matrix.data[i] = [1.0] * len(features_i)
+
+        start_feature_matrix = feature_matrix.tocsr()
+        start_prob = np.exp(start_feature_matrix.dot(weights))
+
+
+        # dist probs
+        d_probs = np.zeros((J - 1, I_ext, I_ext))
+        for j in xrange(1, J):
+            for ip in xrange(I_ext):
+                feature_matrix = lil_matrix((I_ext, feature_dim))
+                for i in xrange(I_ext):
+                    features_i = dist_vecs[feature_ids[j][ip][i]]
+                    feature_matrix.rows[i] = features_i
+                    feature_matrix.data[i] = [1.0] * len(features_i)
+
+                ip_feature_matrix = feature_matrix.tocsr()
+                d_probs[j - 1, ip, :I + 1] = np.exp(ip_feature_matrix.dot(weights))
+
+
+        f = np.zeros((J, I_ext))
+        f_in = np.zeros((J, I_ext))
+        best = np.zeros((J, I_ext), dtype=int)
+
+        for j in reversed(xrange(1, J)):
+            values = (np.log(d_probs[j - 1])) + f_in[j]
+            best_is = np.argmax(values, axis=1)
+            best[j] = best_is
+            f[j] = values[np.arange(I_ext), best_is]
+            f_in[f_heads[j]] += f[j]
+
+        f[0] = np.log(start_prob)  + f_in[0]
+
+        last_best = np.argmax(f[0])
+        alignment = [int(last_best)]
+        for j in range(1, J):
+            head = f_heads[j]
+            if head == 0:
+                alignment.append(best[j][last_best])
+            else:
+                alignment.append(best[j][alignment[head]])
+
+        # alignment = [(al, order[j]) for j, al in enumerate(alignment) if al < I]
+        # alignment = [(al, j) for j, al in enumerate(alignment) if al < I]
+        correct = 0
+        wrong = 0
+        for j in xrange(J):
+            pred = alignment[j]
+            true = gold_aligned[j]
+            if pred == true:
+                correct += 1
+            else:
+                wrong += 1
+
+        result_queue.put((correct, wrong))
+
 
 
 #############################################
@@ -90,13 +163,14 @@ if __name__ == "__main__":
     arg_parser.add_argument("-corpus", required=True)
     arg_parser.add_argument("-weights", required=True)
     arg_parser.add_argument("-vecs", required=True)
-    arg_parser.add_argument("-kappa", required=True, type=float)
+    arg_parser.add_argument("-sigma", required=True, type=float)
     arg_parser.add_argument("-num_workers", required=False, type=int, default=3)
 
     args = arg_parser.parse_args()
     num_workers = max(1, args.num_workers)
 
-    kappa = args.kappa
+    sigma = args.sigma
+    sigmasqrd = np.square(sigma)
 
     global_weights = load_weights(args.weights)
     feature_dim = len(global_weights)
@@ -105,11 +179,36 @@ if __name__ == "__main__":
     process_queue = mp.Queue(maxsize=num_workers*2)
     result_queue = mp.Queue()
 
+    evaluate_queue = mp.Queue(maxsize=num_workers*2)
+    evaluated_queue = mp.Queue()
+
+    def evaluate(d_weights):
+        correct, wrong = 0, 0
+        c = 0
+        for buff in corpus:
+            c += 1
+            evaluate_queue.put((buff, np.array(d_weights)))
+
+        for _ in xrange(c):
+            tmp_correct, tmp_wrong = evaluated_queue.get()
+            correct += tmp_correct
+            wrong += tmp_wrong
+
+        total = correct + wrong
+        return float(correct) / total
+
+
     pool = []
     for w in xrange(num_workers):
         p = mp.Process(target=datapoint_worker, args=(process_queue, result_queue))
         p.start()
         pool.append(p)
+    eval_pool = []
+    for w in xrange(num_workers):
+        p = mp.Process(target=predict, args=(evaluate_queue, evaluated_queue))
+        p.start()
+        eval_pool.append(p)
+
 
     corpus = SubcorpusReader(args.corpus)
     corpus = [buff for buff in corpus]
@@ -129,26 +228,36 @@ if __name__ == "__main__":
             total_ll += -Z + gold_ll
             total_gradient += gradient
 
-        # l2-norm:
-        l2_norm = np.linalg.norm(d_weights)
-        total_ll -= kappa * l2_norm
+        # regularization
+        sm_weights = np.sum(np.square(d_weights))
+        total_ll -= float(sm_weights) / (2*sigmasqrd)
 
-        total_gradient -= 2 * kappa * d_weights
+        total_gradient -= d_weights / sigmasqrd
         return -total_ll, -total_gradient
 
 
     original_weights = np.array(global_weights)
     initial_ll, _ = objective_func(original_weights)
+    initial_acc = evaluate(original_weights)
+
 
     optimized_weights, best_ll, _ = fmin_l_bfgs_b(objective_func, np.array(global_weights), m=10, iprint=1)
     logger.info("Optimization done.")
     logger.info("Initial likelihood: " + str(-initial_ll))
     logger.info("Best likelihood: " + str(-best_ll))
     logger.info("LL diff: " + str(initial_ll - best_ll))
+    final_acc = evaluate(optimized_weights)
+    logger.info("Initial accuracy: " + str(initial_acc))
+    logger.info("Final accuracy: " + str(final_acc))
+
 
     for p in pool:
         process_queue.put(None)
+    for p in eval_pool:
+        evaluate_queue.put(None)
     for p in pool:
+        p.join()
+    for p in eval_pool:
         p.join()
 
 
