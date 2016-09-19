f5cfd7d0117e6cc2ff02f45a319667ede8ebf3ec
Roger Wechsler <roger.wechsler@student.uva.nl>
2016-09-19 21:17:54 +0200

    Bug fix.

--- jobs/crf/lisa/align_all_crf.py
+++ jobs/crf/lisa/align_all_crf.py
@@ -9,7 +9,7 @@ def parse_config(args):
     params = dict()
     params['dir'] = os.path.abspath(args.dir)
     params['fvoc'] = os.path.abspath(glob.glob(os.path.join(params["dir"], "*.fvoc"))[0])
-    params['fvoc'] = os.path.abspath(glob.glob(os.path.join(params["dir"], "*.weights"))[0])
+    params['weights'] = os.path.abspath(glob.glob(os.path.join(params["dir"], "*.weights"))[0])
     params['corpus'] = os.path.abspath(args.corpus)
     params['corpus_dir'] = os.path.dirname(os.path.realpath(params['corpus']))
     params['ibm1_table'] = os.path.abspath(os.path.join(params['corpus_dir'], 'ibm1.table'))
